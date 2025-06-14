8522cd12404126081d047a772e80b04a478e6a3f
Fernando de Gois <fernando.gois90@outlook.com>
2025-06-13 21:39:41 -0300

    Update portfolioDAO.py
    Agora funcionando..
    acrescentei função para gravar o portfolio dos tecnicos, agora podem salvar suas descrições. Tive que trocar id_tecnico para tecnico_id, pois era assim que fiz no banco e estava errado nos codigos

--- portfolioDAO.py
+++ portfolioDAO.py
@@ -1,21 +1,21 @@
-# from database import Database
+from database import Database
 
-# class PortfolioDAO:
-#     def salvar(self, id_tecnico, descricao):
-#         conn = Database.conectar()
-#         cursor = conn.cursor()
-#         sql = "INSERT INTO portfolio (id_tecnico, descricao) VALUES (%s, %s)"
-#         cursor.execute(sql, (id_tecnico, descricao))
-#         conn.commit()
-#         cursor.close()
-#         conn.close()
+class PortfolioDAO:
+    def salvar(self, tecnico_id, descricao):
+        conn = Database.conectar()
+        cursor = conn.cursor()
+        sql = "INSERT INTO portfolio (tecnico_id, descricao) VALUES (%s, %s)"
+        cursor.execute(sql, (tecnico_id, descricao))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
-#     def carregar_por_tecnico(self, id_tecnico):
-#         conn = Database.conectar()
-#         cursor = conn.cursor()
-#         sql = "SELECT descricao FROM portfolio WHERE id_tecnico = %s ORDER BY data_envio DESC"
-#         cursor.execute(sql, (id_tecnico,))
-#         resultados = cursor.fetchall()
-#         cursor.close()
-#         conn.close()
-#         return resultados
+    def carregar_por_tecnico(self, tecnico_id):
+        conn = Database.conectar()
+        cursor = conn.cursor()
+        sql = "SELECT descricao FROM portfolio WHERE tecnico_id = %s ORDER BY data_envio DESC"
+        cursor.execute(sql, (tecnico_id,))
+        resultados = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return resultados
